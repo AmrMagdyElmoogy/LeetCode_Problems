ccf7f223d5a2a4ab6c01cb42be2ed878d8c4bcb4
AmrMagdyElmoogy <47532331+AmrMagdyElmoogy@users.noreply.github.com>
2022-07-27 14:58:39 +0200

    Time: 7 ms (30.72%), Space: 9.8 MB (62.80%) - LeetHub

--- 783-minimum-distance-between-bst-nodes/783-minimum-distance-between-bst-nodes.cpp
+++ 783-minimum-distance-between-bst-nodes/783-minimum-distance-between-bst-nodes.cpp
@@ -11,25 +11,19 @@
  */
 class Solution {
 public:
-    vector<int>v;
+    int result = 10000;
+    int prev = -1;
     int minDiffInBST(TreeNode* root) {
-        preOrder(root); 
-        sort(v.begin(),v.end());
-        int diff = 1000;
-        for(int i=0; i<v.size()-1;i++)
-        {
-            if(v[i+1] - v[i] < diff)
-                diff = v[i+1]-v[i];
-        }
-        return diff;
-    } 
-    void preOrder(TreeNode *root)
-    {
         if(root!=NULL)
         {
-            v.push_back(root->val);
-            preOrder(root->left);
-            preOrder(root->right);
+            
+            minDiffInBST(root->left);
+            if(prev >=0)
+                result = min(result, root->val - prev);
+            prev = root->val;
+            minDiffInBST(root->right);
         }
-    }
+        return result;
+    } 
+    
 };
