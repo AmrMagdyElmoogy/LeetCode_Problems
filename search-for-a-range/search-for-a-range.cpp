9800ba7c0fc9e2f1be1fe48b2276d8cda0a64795
AmrMagdyElmoogy <47532331+AmrMagdyElmoogy@users.noreply.github.com>
2022-08-16 15:06:05 +0200

    Time: 14 ms, Memory: 14.2 MB - LeetHub

--- search-for-a-range/search-for-a-range.cpp
+++ search-for-a-range/search-for-a-range.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        return {BinarySearch(nums,target,false), BinarySearch(nums,target,true)};
-    } 
-    int BinarySearch(vector<int>nums, int target, bool find)
+        return{BinarySearch(nums,target,false),BinarySearch(nums,target,true)};
+    }
+    int BinarySearch(vector<int>nums,int target, bool direction)
     {
         int l = 0;
         int r = nums.size()-1;
@@ -14,13 +14,14 @@ public:
             if(nums[mid] == target)
             {
                 result = mid;
-                find == false ? r = mid - 1 : l = mid + 1;
+                direction == false ? r = mid-1 :  l = mid+1;
             }
+                
             else if(nums[mid] < target)
                 l = mid + 1;
             else 
-                r = mid - 1;
-        } 
-        return result;       
-    }
+                r = mid-1;
+        }
+       return result;
+    } 
 };
