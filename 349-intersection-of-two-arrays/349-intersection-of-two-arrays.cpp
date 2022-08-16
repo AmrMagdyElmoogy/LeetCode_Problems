d998df72c0d4d0113423024ca432c3217ed4edae
AmrMagdyElmoogy <47532331+AmrMagdyElmoogy@users.noreply.github.com>
2022-08-16 15:34:52 +0200

    Time: 12 ms (42.20%), Space: 11.2 MB (5.99%) - LeetHub

--- 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
@@ -2,31 +2,18 @@ class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
         set<int>s;
+        map<int,int>m;
         vector<int>result;
-        sort(nums1.begin(),nums1.end());
-        for(int i=0;i<nums2.size();i++)
-        {
+        for(int i=0; i<nums1.size();i++)
+            m[nums1[i]]++;
+        for(int i=0; i<nums2.size();i++)
             s.insert(nums2[i]);
-        } 
         for(auto it = s.begin();it!=s.end();it++)
         {
-            int target = *it;
-            int l = 0;
-            int r = nums1.size()-1;
-            while(l<=r)
-            {
-                int mid = l+(r-l)/2;
-                if(nums1[mid] == target)
-                {
-                    result.push_back(target);
-                    break;
-                }
-                else if(nums1[mid] < target)
-                    l =  mid + 1;
-                else 
-                    r =  mid - 1;
-            }
-        } 
+            int key = *it;
+            if(m[key]!=0)
+                result.push_back(key);
+        }
         return result;
     }
 };
