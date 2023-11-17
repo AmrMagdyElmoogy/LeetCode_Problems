39939ec2956e8580954cafc588f90da5febb2297
AmrMagdyElmoogy <47532331+AmrMagdyElmoogy@users.noreply.github.com>
2023-11-17 12:28:58 +0200

    Time: 1272 ms (14.13%), Space: 38.5 MB (32.61%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.kt
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.kt
@@ -2,19 +2,26 @@ class Solution {
     fun longestPalindrome(s: String): String {
         val size = s.length
         var result = ""
-        for (i in 0 until size) {
-            for (j in size - 1 downTo i) {
-                val str = s.substring(i, j + 1)
+        val m = mutableMapOf<Char, MutableList<Int>>()
+        s.forEachIndexed { i, it ->
+            m.computeIfAbsent(it) { mutableListOf() }.add(i)
+        }
+        m.values.forEach { value ->
+        for(i in 0 until value.size){
+            for(j in i+1 until value.size){
+                val str = s.substring(value[i],value[j]+1)
                 if (isPalindromic(str)) {
-                    if (result.length < str.length)
+                    if (result.length < str.length) {
                         result = str
+                    }
                 }
             }
+          }
         }
         if (result.isEmpty())
             return s[0].toString()
         return result
-}
+    }
 
     fun isPalindromic(str: String): Boolean {
         val size = str.length
