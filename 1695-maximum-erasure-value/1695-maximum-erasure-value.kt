2ac6ea094f81e122472832264ad83ddabe56732f
AmrMagdyElmoogy <47532331+AmrMagdyElmoogy@users.noreply.github.com>
2023-12-13 09:12:05 +0200

    Time: 665 ms (7.69%), Space: 53.1 MB (46.15%) - LeetHub

---
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.kt
@@ -0,0 +1,23 @@
+class Solution {
+    fun maximumUniqueSubarray(nums: IntArray): Int {
+        val m = mutableMapOf<Int,Int>()
+        var (left,sum) = 0 to 0
+        var maximum = 0
+        val size = nums.size
+        for(i in 0 until size)
+        {
+            m[nums[i]] = m.getOrDefault(nums[i],0)+1
+            sum+= nums[i]
+            if(m[nums[i]] == 1)
+            {
+                maximum = maxOf(maximum,sum)
+            }
+            while(m[nums[i]]!=1){
+                sum-= nums[left]
+                m[nums[left]] = m[nums[left]]!! -1 
+                left++
+            }
+        }
+        return maximum
+    }
+}
