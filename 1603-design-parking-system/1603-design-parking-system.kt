d8f38c2740ef21d9cb17433a79fcdb00c93518d8
Elmoogy <47532331+AmrMagdyElmoogy@users.noreply.github.com>
2024-03-25 14:03:36 +0200

    Time: 281 ms (46.00%), Space: 47.1 MB (46.00%) - LeetHub

--- 1603-design-parking-system/1603-design-parking-system.kt
+++ 1603-design-parking-system/1603-design-parking-system.kt
@@ -1,14 +1,10 @@
 class ParkingSystem(big: Int, medium: Int, small: Int) {
 
-    private var bigSlot = big
-    private var mediumSlot = medium
-    private var smallSlot = small
+    private val hashMap = hashMapOf(1 to big, 2 to medium, 3 to small)
     fun addCar(carType: Int): Boolean {
-        return when (carType) {
-            1 -> bigSlot-- > 0
-            2 -> mediumSlot-- > 0
-            else -> smallSlot-- > 0
-        }
+        val result = hashMap[carType]!! > 0
+        hashMap[carType]= hashMap[carType]!! - 1
+        return result 
     }
 
 }
