896c7e49764b0c0091b1b9f3cfbaca09137c3c84
Elmoogy <47532331+AmrMagdyElmoogy@users.noreply.github.com>
2024-04-08 15:32:56 +0200

    Time: 547 ms (31.25%), Space: 84.7 MB (21.88%) - LeetHub

--- 1352-product-of-the-last-k-numbers/1352-product-of-the-last-k-numbers.kt
+++ 1352-product-of-the-last-k-numbers/1352-product-of-the-last-k-numbers.kt
@@ -1,15 +1,21 @@
 class ProductOfNumbers() {
-    private val list = mutableListOf<Int>()
+    private var prefixMulti = mutableListOf<Int>(1)
     fun add(num: Int) {
-        list.add(num)
-
+        if (num > 0) {
+            prefixMulti.add(prefixMulti.last() * num)
+        } else {
+            prefixMulti = mutableListOf(1)
+        }
     }
 
     fun getProduct(k: Int): Int {
-        return list.takeLast(k).fold(1) { acc, inc -> acc * inc }
+        val size = prefixMulti.size
+        return if (k < size)
+            prefixMulti[size - 1] / prefixMulti[size - k - 1]
+        else
+            0
     }
 }
-
 /**
  * Your ProductOfNumbers object will be instantiated and called as such:
  * var obj = ProductOfNumbers()
