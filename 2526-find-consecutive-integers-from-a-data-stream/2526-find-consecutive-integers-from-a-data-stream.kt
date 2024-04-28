23ef21198f9f176aadc005700a2b4f10a4acbb3d
Elmoogy <47532331+AmrMagdyElmoogy@users.noreply.github.com>
2024-04-28 19:24:49 +0300

    Time: 714 ms (75.00%), Space: 131.5 MB (25.00%) - LeetHub

--- 2526-find-consecutive-integers-from-a-data-stream/2526-find-consecutive-integers-from-a-data-stream.kt
+++ 2526-find-consecutive-integers-from-a-data-stream/2526-find-consecutive-integers-from-a-data-stream.kt
@@ -1,21 +1,10 @@
-import java.util.LinkedList
-
 class DataStream(private val value: Int, private val k: Int) {
-    private val window = LinkedList<Int>()
-
+    private var cnt = 0
     fun consec(num: Int): Boolean {
-        window.offer(num)
-        if (window.size > k) {
-            window.poll()
-        }
-        if (window.size == k) {
-            for (element in window) {
-                if (element != value) {
-                    return false
-                }
-            }
-            return true
-        }
-        return false
+        if(num == value)
+            cnt++
+        else 
+            cnt = 0
+        return cnt>=k
     }
 }
