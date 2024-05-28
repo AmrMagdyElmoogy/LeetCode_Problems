fcd079fd0f605341645a86865c67562b9c2dd51b
Elmoogy <47532331+AmrMagdyElmoogy@users.noreply.github.com>
2024-05-28 14:55:20 +0300

    Time: 174 ms (18.18%), Space: 37.3 MB (45.45%) - LeetHub

--- 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.kt
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.kt
@@ -1,15 +1,28 @@
 class Solution {
     fun specialArray(nums: IntArray): Int {
-        var result = -1
-        nums.forEach {
-            result = max(result, it)
-        }
-
-        (0..result).forEach { number ->
-            if (nums.count { it >= number } == number) {
-                return number
-            }
+        nums.sort()
+        val size = nums.size
+        for (i in 1..size) {
+            val k = getFirstGreaterOrEqual(nums, i)
+            if (size - k == i)
+                return i
         }
         return -1
+    }
+
+    private fun getFirstGreaterOrEqual(nums: IntArray, i: Int): Int {
+        var start = 0
+        var end = nums.size - 1
+        var index = nums.size
+        while (start <= end) {
+            val mid = (start + end) / 2
+            if (nums[mid] >= i) {
+                index = mid
+                end = mid - 1
+            } else
+                start = mid + 1
+        }
+
+        return index
     }
 }
